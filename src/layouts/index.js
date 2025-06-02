62ac84f6082543a46d13ba5aa43432d105ecd66a
Bright Mabhiza <specifixperts@gmail.com>
2025-06-02 03:38:54 +0200

    deploy_2.12 2/6/2025

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,3 @@
-export * from "@/layouts/dashboard";
-export * from "@/layouts/auth";
-export * from "@/layouts/reports";
+export * from "./dashboard";
+export * from "./auth";
+export * from "./reports";
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/material-tailwind-dashboard-react",
+  
   resolve: {
-    alias: [{ find: "@", replacement: ".." }],
+    alias: [{ find: "@", replacement: "/src" }],
   },
   
 });
