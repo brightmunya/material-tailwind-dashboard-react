f9c5fc7d0ecc83616afda1c493e66d6dd6ac536a
Bright Mabhiza <specifixperts@gmail.com>
2025-06-02 03:55:10 +0200

    deploy_2.16 2/6/2025

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
-import { MaterialTailwindControllerProvider } from "@/context";
+import { MaterialTailwindControllerProvider } from "./context";
 import "../public/css/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
