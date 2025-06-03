88691529cf803e5eac94f26e612da3cb0c91a61d
Bright Mabhiza <specifixperts@gmail.com>
2025-06-03 03:15:03 +0200

    deployment 1

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/material-tailwind-dashboard-react",
+
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
   
 });
+// for git-pages deployment, replace the base path with your repository name
+//   base: "/material-tailwind-dashboard-react",
+  
